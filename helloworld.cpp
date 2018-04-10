ed8f95b2b2bf2f09435699163cfbd55cbfa18a5f
Alexander <38082192+Alexander-novichok@users.noreply.github.com>
2018-04-11 00:57:38 +0300

    Добавление файла

---
+++ helloworld.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    
+    cout << "\nHello World!\n";
+    cout << "что ты такое?\n" << endl;
+    cout << "кАК у тебя дела ?" << endl;
+    puts ("Я нормально!");
+    puts ("Проверка Git_модификация  ");
+    puts ("Проверка: git diff - просмотр изм. no add file");
+     puts ("Проверка: git commit - слепок файлов");
+     puts ("Проверка: git commit -a -m 'место для комментария' - слепок файлов c индексащией и комментарием в терминале");
+     puts ("Проверка: git: - проверка работы с удаленным репозиторием 00:26/11.04.18");
+     
+    return 0;
+}
